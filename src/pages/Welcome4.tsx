e862fb31cafa2da161e5d3e8d4bfde9d526124f2
hukai <huzhengen@163.com>
2022-11-02 23:45:07 +0800

    Add react-spring

--- src/pages/Welcome4.tsx
+++ src/pages/Welcome4.tsx
@@ -2,6 +2,6 @@ import * as React from 'react'
 import { NavLink } from 'react-router-dom'
 export const Welcome4: React.FC = () => {
   return (
-    <div style={{ border: '1px solid red' }}> 4 <NavLink to="/welcome/3">下一页</NavLink> </div>
+    <div style={{ border: '1px solid red' }}> 4 <NavLink to="/welcome/xxx">下一页</NavLink> </div>
   )
 }
