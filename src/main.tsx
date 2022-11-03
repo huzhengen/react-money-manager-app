fc7abdfa4df939e2492f9725a8cedbeb18630ae0
hukai <huzhengen@163.com>
2022-11-03 20:15:33 +0800

    Add unocss

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/router'
+import 'virtual:uno.css'
 
 const div = document.getElementById('root') as HTMLElement
 
 const root = ReactDOM.createRoot(div)
+
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
