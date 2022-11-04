0159b145788d51274579e8c944b241cb2dcbb941
hukai <huzhengen@163.com>
2022-11-04 18:13:15 +0800

    Add NotFoundPage

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,12 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom'
 import { MainLayout } from '../layouts/MainLayout'
+import { NotFoundPage } from '../pages/NotFoundPage'
 import { welcomeRoutes } from './welcomeRoutes'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <MainLayout />,
-    errorElement: <div>Not Found</div>,
+    errorElement: <NotFoundPage />,
     children: [
       welcomeRoutes
     ],
