0117bf9ad38404c5e5a9f881edf4e123091e691b
huzhengen <huzhengen@163.com>
2022-11-26 20:56:54 +0800

    Update TimeRangePicker

--- src/components/TimeRangePicker.tsx
+++ src/components/TimeRangePicker.tsx
@@ -4,13 +4,12 @@ interface Props {
   selected: TimeRange
   onSelected: (selected: TimeRange) => void
 }
+const timeRanges: { key: TimeRange; text: string }[] = [
+  { key: 'thisMonth', text: 'this month' },
+  { key: 'thisYear', text: 'this year' },
+  { key: 'custom', text: 'custom' },
+]
 export const TimeRangePicker: React.FC<Props> = ({ selected, onSelected }) => {
-  const timeRanges: { key: TimeRange; text: string }[] = [
-    { key: 'thisMonth', text: 'this month' },
-    { key: 'thisYear', text: 'this year' },
-    { key: 'custom', text: 'custom' },
-  ]
-
   return <ol flex text-white children-px-24px children-py-12px cursor-pointer>
     {timeRanges.map(tr => <li key={tr.key} className={tr.key === selected ? s.selected : ''}
     onClick={() => onSelected(tr.key)}>
