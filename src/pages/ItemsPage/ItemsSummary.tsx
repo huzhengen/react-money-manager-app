584234788bac5ebfa953c05aede86f3d59f572db
huzhengen <huzhengen@163.com>
2022-11-28 20:49:42 +0800

    ItemSummary

--- src/pages/ItemsPage/ItemsSummary.tsx
+++ src/pages/ItemsPage/ItemsSummary.tsx
@@ -1,3 +1,19 @@
 export const ItemsSummary: React.FC = () => {
-  return <div>ItemsSummary</div>
+  return (
+    <ol bg="#252A43" flex justify-between items-center m-16px rounded-8px py-12px px-16px
+      children-px-24px text-center>
+      <li text="#FE7275">
+        <div>Income</div>
+        <div>1000</div>
+      </li>
+      <li text="#53A867">
+        <div>Expenses</div>
+        <div>1000</div>
+      </li>
+      <li text-white>
+        <div>Balance</div>
+        <div>1000</div>
+      </li>
+    </ol>
+  )
 }
