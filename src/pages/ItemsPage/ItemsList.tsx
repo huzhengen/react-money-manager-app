004e3c2be1418e720b4eda3f9e3c81cc663bee6b
huzhengen <huzhengen@163.com>
2022-11-28 22:33:34 +0800

    Complete ItemList

--- src/pages/ItemsPage/ItemsList.tsx
+++ src/pages/ItemsPage/ItemsList.tsx
@@ -1,3 +1,28 @@
-export const ItemsList: React.FC = () => {
-  return <div>ItemsList</div>
+interface Props {
+  items: Item[]
+}
+export const ItemsList: React.FC<Props> = ({ items }) => {
+  return <div>
+    <ol>
+      {items.map(item => <li key={item.id} grid grid-cols="[auto_1fr_auto]" grid-rows-2 px-16px py-8px gap-x-12px
+          border-b-1 b="#EEE">
+        <div row-start-1 col-start-1 row-end-3 col-end-2 text-24px w-48px h-48px
+          bg="#D8D8D8" rounded="50%" flex justify-center items-center>
+          😘
+        </div>
+        <div row-start-1 col-start-2 row-end-2 col-end-3>
+          Travel
+        </div>
+        <div row-start-2 col-start-2 row-end-3 col-end-4 text="#999999">
+          1/1/2022
+        </div>
+        <div row-start-1 col-start-3 row-end-2 col-end-4 text="#53A867">
+          $999
+        </div>
+      </li>)}
+    </ol>
+    <div p-16px>
+      <button j-btn>More</button>
+    </div>
+  </div>
 }
