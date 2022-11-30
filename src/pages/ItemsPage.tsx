e3ca835acb10537026e9d8ec10f3389731f0abb6
huzhengen <huzhengen@163.com>
2022-11-30 14:22:54 +0800

    Animation of the menu

--- src/pages/ItemsPage.tsx
+++ src/pages/ItemsPage.tsx
@@ -45,6 +45,6 @@ export const ItemsPage: React.FC = () => {
     <ItemsSummary />
     <ItemsList items={items} />
     <AddItemFloatButton />
-    {visible ? <TopMenu onClickMask={() => setVisible(false)} /> : null}
+    <TopMenu visible={visible} onClickMask={() => setVisible(false)} />
   </div>
 }
