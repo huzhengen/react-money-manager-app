bf9bdf12bba0d179ccb7e3a853777ba929739a90
huzhengen <huzhengen@163.com>
2022-12-01 21:51:20 +0800

    Render mock data using useSWRInfinite

--- src/pages/ItemsPage/ItemsList.tsx
+++ src/pages/ItemsPage/ItemsList.tsx
@@ -6,13 +6,36 @@ const getKey = (pageIndex: number) => {
   return `/api/v1/items?page=${pageIndex + 1}`
 }
 export const ItemsList: React.FC<Props> = () => {
-  const { data, error } = useSWRInfinite(
-    getKey, async path => (await ajax.get<Resource<Item>>(path)).data
+  const { data, error, size, setSize } = useSWRInfinite(
+    getKey, async path => (await ajax.get<Resources<Item>>(path)).data
   )
   if (!data) {
-    return 'no data'
+    return <div>no data</div>
   } else {
-    console.log(data, error)
+    return <><ol>{data.map(({ resources }) => {
+      return resources.map(item =>
+        <li key={item.id} grid grid-cols="[auto_1fr_auto]" grid-rows-2 px-16px py-8px gap-x-12px
+          border-b-1 b="#EEE">
+          <div row-start-1 col-start-1 row-end-3 col-end-2 text-24px w-48px h-48px
+            bg="#D8D8D8" rounded="50%" flex justify-center items-center>
+            😘
+          </div>
+          <div row-start-1 col-start-2 row-end-2 col-end-3>
+            旅行
+          </div>
+          <div row-start-2 col-start-2 row-end-3 col-end-4 text="#999999">
+            2011年1月1日
+          </div>
+          <div row-start-1 col-start-3 row-end-2 col-end-4 text="#53A867">
+            ￥{item.amount / 100}
+          </div>
+        </li>
+      )
+    })}</ol>
+      <div p-16px>
+        <button j-btn>Load more</button>
+      </div>
+    </>
   }
   const items: Item[] = []
   return <div>
