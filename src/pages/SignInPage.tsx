4b3bc467c21ac85c47137a55c24673877ab57174
huzhengen <huzhengen@163.com>
2022-12-03 17:10:04 +0800

    useSignInStore

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,8 +1,14 @@
+import type { FormEventHandler } from 'react'
 import { Gradient } from '../components/Gradient'
 import { Icon } from '../components/Icon'
 import { TopNav } from '../components/TopNav'
+import { useSignInStore } from '../stores/useSignInStore'
 
 export const SignInPage: React.FC = () => {
+  const { data, setData } = useSignInStore()
+  const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault()
+  }
   return (<div>
     <Gradient>
       <TopNav title='Sign In' icon={<Icon name="back" onClick={() => { }} />} />
@@ -11,15 +17,17 @@ export const SignInPage: React.FC = () => {
       <Icon name="logo" className='w-64px h-68px' />
       <h1 text-32px text="#7878FF" font-bold>Money Manager</h1>
     </div>
-    <form j-form>
+    <form j-form onSubmit={onSubmit}>
       <div>
         <span j-form-label>Email</span>
-        <input j-input-text type="text" placeholder='Email' />
+        <input j-input-text type="text" placeholder='Email'
+          value={data.email} onChange={e => setData({ email: e.target.value })} />
       </div>
       <div>
         <span j-form-label>Code</span>
         <div flex gap-x-16px>
-          <input j-input-text type="text" placeholder='Enter 6-digit code' />
+          <input j-input-text type="text" placeholder='Enter 6-digit code'
+            value={data.code} onChange={e => setData({ code: e.target.value })} />
           <button j-btn>Send Code</button>
         </div>
       </div>
---
+++ src/stores/useSignInStore.tsx
@@ -0,0 +1,21 @@
+import create from 'zustand'
+
+interface Data {
+  email: string
+  code: string
+}
+
+interface SignIn {
+  data: Data
+  setData: (data: Partial<Data>) => void
+}
+
+export const useSignInStore = create<SignIn>(set => ({
+  data: {
+    email: '',
+    code: ''
+  },
+  setData: (data: Partial<Data>) => {
+    set(state => ({ data: { ...state.data, ...data } }))
+  },
+}))
