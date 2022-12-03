b332cd0ee32e0210f1dd561e8cc5c03adbc29831
huzhengen <huzhengen@163.com>
2022-12-03 21:09:33 +0800

    Form error

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -2,12 +2,20 @@ import type { FormEventHandler } from 'react'
 import { Gradient } from '../components/Gradient'
 import { Icon } from '../components/Icon'
 import { TopNav } from '../components/TopNav'
+import { validate } from '../lib/validate'
 import { useSignInStore } from '../stores/useSignInStore'
 
 export const SignInPage: React.FC = () => {
-  const { data, setData } = useSignInStore()
+  const { data, error, setData, setError } = useSignInStore()
   const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault()
+    const error = validate(data, [
+      { key: 'email', type: 'required', message: 'Please input your email' },
+      { key: 'email', type: 'pattern', regex: /^.+@.+$/, message: 'The email is incorrect' },
+      { key: 'code', type: 'required', message: 'Please input your code' },
+      { key: 'code', type: 'length', min: 6, max: 6, message: 'The code is incorrect' },
+    ])
+    setError(error)
   }
   return (<div>
     <Gradient>
@@ -19,12 +27,12 @@ export const SignInPage: React.FC = () => {
     </div>
     <form j-form onSubmit={onSubmit}>
       <div>
-        <span j-form-label>Email</span>
+        <span j-form-label>Email {error.email?.[0] && <span text-red>{error.email[0]}</span>}</span>
         <input j-input-text type="text" placeholder='Email'
           value={data.email} onChange={e => setData({ email: e.target.value })} />
       </div>
       <div>
-        <span j-form-label>Code</span>
+        <span j-form-label>Code {error.code?.[0] && <span text-red>{error.code[0]}</span>}</span>
         <div flex gap-x-16px>
           <input j-input-text type="text" placeholder='Enter 6-digit code'
             value={data.code} onChange={e => setData({ code: e.target.value })} />
--- src/stores/useSignInStore.tsx
+++ src/stores/useSignInStore.tsx
@@ -1,13 +1,16 @@
 import create from 'zustand'
+import type { FormError } from '../lib/validate'
 
-interface Data {
+type Data = {
   email: string
   code: string
 }
 
 interface SignIn {
   data: Data
+  error: FormError<Data>
   setData: (data: Partial<Data>) => void
+  setError: (error: Partial<FormError<Data>>) => void
 }
 
 export const useSignInStore = create<SignIn>(set => ({
@@ -15,7 +18,25 @@ export const useSignInStore = create<SignIn>(set => ({
     email: '',
     code: ''
   },
-  setData: (data: Partial<Data>) => {
-    set(state => ({ data: { ...state.data, ...data } }))
+  error: {
+    email: [],
+    code: []
   },
+  setData: (data: Partial<Data>) => {
+    set(state => ({
+      ...state,
+      data: {
+        ...state.data,
+        ...data
+      }
+    }))
+  },
+  setError: (error: Partial<FormError<Data>>) => {
+    set(state => ({
+      ...state,
+      error: {
+        ...error
+      }
+    }))
+  }
 }))
