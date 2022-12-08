27be1019075bdf379035d6f98a0008a536377737
huzhengen <huzhengen@163.com>
2022-12-08 18:05:41 +0800

    Remove cursor-pointer

--- src/components/TimeRangePicker.tsx
+++ src/components/TimeRangePicker.tsx
@@ -11,7 +11,7 @@ const timeRanges: { key: TimeRange; text: string }[] = [
 ]
 export const TimeRangePicker: React.FC<Props> = ({ selected, onSelected }) => {
   return <ol flex text-white children-px-24px children-py-12px>
-    {timeRanges.map(tr => <li cursor-pointer key={tr.key} className={tr.key === selected ? s.selected : ''}
+    {timeRanges.map(tr => <li key={tr.key} className={tr.key === selected ? s.selected : ''}
     onClick={() => onSelected(tr.key)}>
       {tr.text}
     </li>)}
