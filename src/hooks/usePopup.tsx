64ad43bffe3c44b19b7fd7c796ded7a1db766331
huzhengen <huzhengen@163.com>
2023-01-25 18:25:29 +0800

    Put popup in rootDiv

--- src/hooks/usePopup.tsx
+++ src/hooks/usePopup.tsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
+import ReactDOM from 'react-dom'
 import { Popup } from '../components/Popup'
+import { rootDiv } from '../main'
 
 export const usePopup = () => {
   const [visible, setVisible] = useState(false)
 
-  const popup = <Popup visible={visible} onClickMask={() => setVisible(false)} />
+  const popup = ReactDOM.createPortal(
+    <Popup visible={visible} onClickMask={() => setVisible(false)} />,
+    rootDiv
+  )
 
   return {
     popup,
