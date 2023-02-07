1c14ab960fb7da31e2db9b91d3664f46b397402a
huzhengen <huzhengen@163.com>
2023-02-07 15:17:18 +0800

    Update DateAndAmount.tsx

--- src/pages/ItemNewPage/DateAndAmount.tsx
+++ src/pages/ItemNewPage/DateAndAmount.tsx
@@ -25,12 +25,12 @@ export const DateAndAmount: React.FC<Props> = (props) => {
       }
     }}
     onTouchEnd={() => {
-      const yushu = translateY % 36
-      if (Math.abs(yushu) < 18) {
-        setTranslateY(translateY - yushu)
-      } else {
-        setTranslateY(translateY - yushu + 36 * (yushu > 0 ? 1 : -1))
+      const remainder = translateY % 36
+      let y = translateY - remainder
+      if (Math.abs(remainder) > 18) {
+        y += 36 * (remainder > 0 ? 1 : -1)
       }
+      setTranslateY(y)
       setIsTouching(false)
     }}
   >
@@ -49,17 +49,21 @@ export const DateAndAmount: React.FC<Props> = (props) => {
         <li>2008</li>
         <li>2009</li>
         <li>2010</li>
-        <li>2000</li>
-        <li>2001</li>
-        <li>2002</li>
-        <li>2003</li>
-        <li>2004</li>
-        <li>2005</li>
-        <li>2006</li>
-        <li>2007</li>
-        <li>2008</li>
-        <li>2009</li>
-        <li>2010</li>
+        <li>2011</li>
+        <li>2012</li>
+        <li>2013</li>
+        <li>2014</li>
+        <li>2015</li>
+        <li>2016</li>
+        <li>2017</li>
+        <li>2018</li>
+        <li>2019</li>
+        <li>2020</li>
+        <li>2021</li>
+        <li>2022</li>
+        <li>2023</li>
+        <li>2024</li>
+        <li>2025</li>
       </ol>
     </div>
   </div >)
