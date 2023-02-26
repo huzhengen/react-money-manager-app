d24790ab1bd54a870d741cdc785640945c98d154
huzhengen <huzhengen@163.com>
2023-02-26 16:52:00 +0800

    Update Input.tsx

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -11,17 +11,22 @@ type Props = {
 
 export const Input: React.FC<Props> = (props) => {
   const { label, placeholder, type = 'text', value, onChange, error } = props
-
+  const renderInput = () => {
+    switch (type) {
+      case 'text':
+        return <input j-input-text type={type} placeholder={placeholder}
+          value={value} onChange={e => onChange?.(e.target.value)} />
+      case 'emoji':
+        return <EmojiInput />
+      default:
+        return null
+    }
+  }
   return (
     <>
       <div flex flex-col gap-y-8px>
         <span text-18px>{label}</span>
-        {type === 'text'
-          ? <input j-input-text type={type} placeholder={placeholder}
-            value={value} onChange={e => onChange?.(e.target.value)} />
-          : type === 'emoji'
-            ? <EmojiInput />
-            : null}
+        {renderInput()}
         <span text-red text-12px>{error || '\u00A0'}</span>
       </div>
     </>
