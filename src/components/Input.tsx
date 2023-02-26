a4c9303bd460fc61e8a5947e147b4e99fceef43a
huzhengen <huzhengen@163.com>
2023-02-26 16:47:59 +0800

    Create EmojiInput.tsx

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,21 +1,29 @@
+import { EmojiInput } from './Input/EmojiInput'
+
 type Props = {
   label: string
   placeholder?: string
-  type?: 'text'
+  type?: 'text' | 'emoji'
   value?: string
   onChange?: (value: string) => void
   error?: string
 }
 
 export const Input: React.FC<Props> = (props) => {
-  const { label, placeholder, type, value, onChange, error } = props
+  const { label, placeholder, type = 'text', value, onChange, error } = props
+
   return (
-    <div flex flex-col gap-y-8px>
-      <span text-18px>{label}</span>
-      <input j-input-text type={type} placeholder={placeholder} value={value}
-        onChange={e => onChange?.(e.target.value)} />
-      <span text-red text-12px>{error || '\u00A0'}</span>
-    </div>
+    <>
+      <div flex flex-col gap-y-8px>
+        <span text-18px>{label}</span>
+        {type === 'text'
+          ? <input j-input-text type={type} placeholder={placeholder}
+            value={value} onChange={e => onChange?.(e.target.value)} />
+          : type === 'emoji'
+            ? <EmojiInput />
+            : null}
+        <span text-red text-12px>{error || '\u00A0'}</span>
+      </div>
+    </>
   )
 }
-
