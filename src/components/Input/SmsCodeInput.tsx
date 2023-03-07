dabd5eff048f4f53a6ceaf66382674677233083a
huzhengen <huzhengen@163.com>
2023-03-07 12:41:40 +0800

    Countdown

--- src/components/Input/SmsCodeInput.tsx
+++ src/components/Input/SmsCodeInput.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react'
+
 type Props = {
   placeholder?: string
   value?: string
@@ -7,9 +9,36 @@ type Props = {
 
 export const SmsCodeInput: React.FC<Props> = (props) => {
   const { placeholder, value, onChange, request } = props
+  const [started, setStarted] = useState(false)
+  const [count, setCount] = useState(60)
+  useEffect(() => {
+    if (!started) { return }
+
+    const timer = window.setTimeout(() => {
+      setCount(count - 1)
+    }, 1000)
+
+    return () => window.clearTimeout(timer)
+  }, [started, count])
+
+  useEffect(() => {
+    if (count === 0) {
+      setStarted(false)
+      setCount(60)
+      return
+    }
+    if (!started) { return }
+
+    const timer = window.setTimeout(() => {
+      setCount(count - 1)
+    }, 1000)
+
+    return () => window.clearTimeout(timer)
+  }, [count])
   const onClick = async () => {
     if (!request) { return }
     await request()
+    setStarted(true)
   }
   return (
     <div flex gap-x-16px>
