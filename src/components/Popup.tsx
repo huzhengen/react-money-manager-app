923b5e256c154a8527c8d298f770c6ca1a29bac0
huzhengen <huzhengen@163.com>
2023-03-26 20:09:07 +0800

    Update Popup.tsx

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -13,6 +13,7 @@ export const Popup: React.FC<Props> = (props) => {
   const { visible, onClickMask, children, position = 'bottom' } = props
   const [maskVisible, setMaskVisible] = useState(visible)
   const maskStyles = useSpring({
+    visibility: maskVisible ? 'visible' : 'hidden' as 'visible' | 'hidden',
     opacity: visible ? 1 : 0,
     onStart: ({ value }) => {
       if (value.opacity < 0.1) { setMaskVisible(true) }
@@ -22,20 +23,17 @@ export const Popup: React.FC<Props> = (props) => {
     }
   })
   const wrapperStyles = useSpring({
+    visibility: visible ? 'visible' : 'hidden' as 'visible' | 'hidden',
     opacity: visible ? 1 : 0,
     transform: position === 'bottom'
       ? (visible ? 'translateY(0%)' : 'translateY(100%)')
       : '',
   })
-  const maskStyles2 = {
-    ...maskStyles,
-    visibility: (maskVisible ? 'visible' : 'hidden') as 'visible' | 'hidden'
-  } // workaround
   return (
     <div touch-none>
       <animated.div fixed top-0 left-0 h-full w-full className="bg-black:75"
         z="[calc(var(--z-popup)-1)]" onClick={() => onClickMask?.()}
-        style={maskStyles2} />
+        style={maskStyles} />
       {position === 'bottom'
         ? (
           <animated.div fixed bottom-0 left-0 w-full min-h-100px bg-white
