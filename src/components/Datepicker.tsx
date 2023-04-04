d13b88674883a4112bb5130ee4dbdddc9475944c
huzhengen <huzhengen@163.com>
2023-04-04 19:47:35 +0800

    Select hour and minute for bookkeeping

--- src/components/Datepicker.tsx
+++ src/components/Datepicker.tsx
@@ -9,14 +9,14 @@ type Props = {
   onConfirm?: (value: Date) => void
 }
 
-const getNow = () => time().set({ hours: 0, minutes: 0, seconds: 0, ms: 0 })
+const getNow = () => time()
 
 export const Datepicker: React.FC<Props> = (props) => {
   const { start, end, value, onCancel, onConfirm } = props
   const [, update] = useState({})
   const startTime = start ? time(start) : getNow().add(-10, 'years')
   const endTime = end ? time(end) : getNow().add(10, 'year')
-  const valueTime = useRef(value ? time(value).set({ hours: 0, minutes: 0, seconds: 0, ms: 0 }) : getNow())
+  const valueTime = useRef(value ? time(value) : getNow())
   if (endTime.timestamp <= startTime.timestamp) {
     throw new Error('The end time must be later than the start time!')
   }
@@ -25,12 +25,21 @@ export const Datepicker: React.FC<Props> = (props) => {
   const monthList = Array.from({ length: 12 }).map((_, index) => index + 1)
   const dayList = Array.from({ length: valueTime.current.lastDayOfMonth.day })
     .map((_, index) => index + 1)
+  const hoursList = Array.from({ length: 24 }).map((_, index) => index)
+  const minutesList = Array.from({ length: 60 }).map((_, index) => index)
   return (
     <div>
       <div flex justify-between p-8px border-b-1 b-b-solid b="#f3f3f3" children-p-8px>
         <span onClick={onCancel}>Cancel</span>
         <span>Select date</span>
         <span onClick={() => onConfirm?.(valueTime.current.date)}>Confirm</span>
+      </div>
+      <div flex children-grow-1 text-center children-p-16px>
+        <span>Year</span>
+        <span>Month</span>
+        <span>Day</span>
+        <span>Hour</span>
+        <span>Minute</span>
       </div>
       <div flex>
         <Column className="grow-1" items={yearList} value={valueTime.current.year}
@@ -39,6 +48,10 @@ export const Datepicker: React.FC<Props> = (props) => {
           onChange={(month) => { valueTime.current.month = month; update({}) }} />
         <Column className="grow-1" items={dayList} value={valueTime.current.day}
           onChange={(day) => { valueTime.current.day = day; update({}) }} />
+        <Column className="grow-1" items={hoursList} value={valueTime.current.hours}
+          onChange={(hour) => { valueTime.current.hours = hour; update({}) }} />
+        <Column className="grow-1" items={minutesList} value={valueTime.current.minutes}
+          onChange={(minute) => { valueTime.current.minutes = minute; update({}) }} />
       </div>
     </div>
   )
